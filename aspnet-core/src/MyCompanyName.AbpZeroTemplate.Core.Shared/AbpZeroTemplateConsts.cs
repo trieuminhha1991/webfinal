be2566a5e496f2439100bd5e73bebe2ab0fdb9f5
trieuminhha1991 <namviet25791@gmail.com>
2019-11-18 09:54:02 +0700

    1. Mở đầu

---
+++ aspnet-core/src/MyCompanyName.AbpZeroTemplate.Core.Shared/AbpZeroTemplateConsts.cs
@@ -0,0 +1,19 @@
+﻿namespace MyCompanyName.AbpZeroTemplate
+{
+    public class AbpZeroTemplateConsts
+    {
+        public const string LocalizationSourceName = "AbpZeroTemplate";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+        public const bool AllowTenantsToChangeEmailSettings = false;
+
+        public const string Currency = "USD";
+
+        public const string CurrencySign = "$";
+
+        public const string AbpApiClientUserAgent = "AbpApiClient";
+    }
+}
