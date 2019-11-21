f8caf7c61d81b8604b9a3aa80e24348c12cc4333
trieuminhha1991 <namviet25791@gmail.com>
2019-11-21 10:39:36 +0700

    heheheeheh

--- angular/src/app/main/ebook/shop-ebook/shop-ebook.component.ts
+++ angular/src/app/main/ebook/shop-ebook/shop-ebook.component.ts
@@ -1,6 +1,13 @@
-import { Component, Injector } from '@angular/core';
+import {Component, Injector, ViewChild} from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+import {LazyLoadEvent} from "@node_modules/primeng/components/common/lazyloadevent";
+import {PbEbooksServiceProxy, GetPbEbookForViewDto, TokenAuthServiceProxy} from '@shared/service-proxies/service-proxies';
+import {NotifyService} from "@abp/notify/notify.service";
+import {ActivatedRoute} from "@angular/router";
+import {FileDownloadService} from "@shared/utils/file-download.service";
+import * as moment from "@node_modules/moment";
+import {Paginator} from 'primeng/components/paginator/paginator';
 
 @Component({
   selector: 'app-shop-ebook',
@@ -9,9 +16,90 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
     animations: [appModuleAnimation()]
 })
 export class ShopEbookComponent extends AppComponentBase {
+    @ViewChild('paginator', {static: true}) paginator: Paginator;
+    Ebook: GetPbEbookForViewDto[] = [];
+    advancedFiltersAreShown = false;
+    filterText = '';
+    ebookNameFilter = '';
+    linkFilter = '';
+    maxEbookDateStartFilter: moment.Moment;
+    minEbookDateStartFilter: moment.Moment;
+    proFilter = -1;
+    maxEbookPriceFilter: number;
+    maxEbookPriceFilterEmpty: number;
+    minEbookPriceFilter: number;
+    minEbookPriceFilterEmpty: number;
+    maxEbookViewFilter: number;
+    maxEbookViewFilterEmpty: number;
+    minEbookViewFilter: number;
+    minEbookViewFilterEmpty: number;
+    maxEbookLikeFilter: number;
+    maxEbookLikeFilterEmpty: number;
+    minEbookLikeFilter: number;
+    minEbookLikeFilterEmpty: number;
+    maxEbookDislikeFilter: number;
+    maxEbookDislikeFilterEmpty: number;
+    minEbookDislikeFilter: number;
+    minEbookDislikeFilterEmpty: number;
+    discriptionFilter = '';
+    ebookCoverFilter = '';
+    maxBookPageFilter: number;
+    maxBookPageFilterEmpty: number;
+    minBookPageFilter: number;
+    minBookPageFilterEmpty: number;
+    userNameFilter = '';
+    pbClassClassNameFilter = '';
+    pbPlacePlaceNameFilter = '';
+    pbRankRankNameFilter = '';
+    pbStatusStatusNameFilter = '';
+    pbSubjectSectionNameFilter = '';
+    pbSubjectEducationSubjectNameFilter = '';
+    pbTypeEbookTypeNameFilter = '';
+    pbTypeFileTypeFileNameFilter = '';
     constructor(
-        injector: Injector
+        injector: Injector,
+        private _pbEbooksServiceProxy: PbEbooksServiceProxy,
+        private _notifyService: NotifyService,
+        private _tokenAuth: TokenAuthServiceProxy,
+        private _activatedRoute: ActivatedRoute,
+        private _fileDownloadService: FileDownloadService
     ) {
         super(injector);
+    }
+    getPbEbooks() {
+        this._pbEbooksServiceProxy.getAll(
+            this.filterText,
+            this.ebookNameFilter,
+            this.linkFilter,
+            this.maxEbookDateStartFilter,
+            this.minEbookDateStartFilter,
+            this.proFilter,
+            this.maxEbookPriceFilter == null ? this.maxEbookPriceFilterEmpty : this.maxEbookPriceFilter,
+            this.minEbookPriceFilter == null ? this.minEbookPriceFilterEmpty : this.minEbookPriceFilter,
+            this.maxEbookViewFilter == null ? this.maxEbookViewFilterEmpty : this.maxEbookViewFilter,
+            this.minEbookViewFilter == null ? this.minEbookViewFilterEmpty : this.minEbookViewFilter,
+            this.maxEbookLikeFilter == null ? this.maxEbookLikeFilterEmpty : this.maxEbookLikeFilter,
+            this.minEbookLikeFilter == null ? this.minEbookLikeFilterEmpty : this.minEbookLikeFilter,
+            this.maxEbookDislikeFilter == null ? this.maxEbookDislikeFilterEmpty : this.maxEbookDislikeFilter,
+            this.minEbookDislikeFilter == null ? this.minEbookDislikeFilterEmpty : this.minEbookDislikeFilter,
+            this.discriptionFilter,
+            this.ebookCoverFilter,
+            this.maxBookPageFilter == null ? this.maxBookPageFilterEmpty : this.maxBookPageFilter,
+            this.minBookPageFilter == null ? this.minBookPageFilterEmpty : this.minBookPageFilter,
+            this.userNameFilter,
+            this.pbClassClassNameFilter,
+            this.pbPlacePlaceNameFilter,
+            this.pbRankRankNameFilter,
+            this.pbStatusStatusNameFilter,
+            this.pbSubjectSectionNameFilter,
+            this.pbSubjectEducationSubjectNameFilter,
+            this.pbTypeEbookTypeNameFilter,
+            this.pbTypeFileTypeFileNameFilter,
+            null,
+            10,
+            100
+        ).subscribe(result => {
+            this.Ebook=result.items;
+        });
     }
 }
